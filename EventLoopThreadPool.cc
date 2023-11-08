25045808dc97edb5b06fd1dfe89d6aff1684f530
kakaxi <kakaxi_2018@126.com>
2023-11-08 11:08:17 +0800

    add EventLoopThreadPool.cc

---
+++ EventLoopThreadPool.cc
@@ -0,0 +1,71 @@
+#include "EventLoopThreadPool.h"
+#include "EventLoopThread.h"
+#include <memory>
+
+EventLoopThreadPool::EventLoopThreadPool(EventLoop *baseLoop, const std::string &name)
+    : baseLoop_(baseLoop),
+       name_(name),
+       started_(false),
+       numThreads_(0),
+       next_(0)
+{
+}
+
+EventLoopThreadPool::~EventLoopThreadPool()
+{
+    // 不需要 delete loop，线程中绑定的对象都是栈上的对象
+}
+
+void EventLoopThreadPool::start(const ThreadInitCallback &cb)
+{
+    started_ = true;
+
+    for (int i = 0; i < numThreads_; ++i)
+    {
+        char buf[name_.size() + 32];
+        snprintf(buf, sizeof(buf), "%s%d", name_.c_str(), i);
+        EventLoopThread * t = new EventLoopThread(cb, buf);
+        threads_.push_back(std::unique_ptr<EventLoopThread>(t));
+        loops_.push_back(t->startLoop());  // 底层创建线程，绑定一个新的 EventLoop，并返回该 loop 的地址
+    }
+
+    // 整个服务端只有一个线程，运行着 baseloop 
+    if (0 == numThreads_ && cb)
+    {
+        cb(baseLoop_);
+    }
+}
+
+// 如果工作在多线程中，baseLoop 默认以轮询的方式分配 channel 给 subloop
+EventLoop * EventLoopThreadPool::getNextLoop()
+{
+    EventLoop * loop = baseLoop_;
+
+    // 如果没有设置底层线程数量，则返回的是用户创建的 loop，用的就是 baseLoop
+    if (!loops_.empty())
+    {
+        // round-robin
+        // 通过轮询获取下一个处理事件的 loop
+        loop = loops_[next_];
+        ++next_;
+
+        if (next_ >= loops_.size())
+        {
+            next_ = 0;
+        }
+    }
+
+    return loop;
+}
+
+std::vector<EventLoop *> EventLoopThreadPool::getAllLoops()
+{
+    if (loops_.empty())
+    {
+        return std::vector<EventLoop *>(1, baseLoop_);
+    }
+    else
+    {
+        return loops_;
+    }
+}
